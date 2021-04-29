4cc334189a82f78ef0781fee31bb93cacd528b58
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-04-29 22:55:55 +0200

    add copyright

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <ParentLayout>
+        <template #page-bottom>
+            <footer class="footer">
+                <div class="copyright">
+                    ©2021-present <a href="https://www.stefanobartoletti.it" target="_blank">Stefano Bartoletti</a>
+                </div>
+                <div class="icons">
+                    
+                </div>
+            </footer>
+        </template>
+    </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    ParentLayout
+  }
+}
+</script>
